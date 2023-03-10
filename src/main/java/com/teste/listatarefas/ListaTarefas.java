b32d9a54137b8d9b39ab0ba97bccfddfa8186f32
LaraGuedes <lara.guedes569@gmail.com>
2023-03-10 20:35:22 -0300

    Projeto pronto

---
+++ src/main/java/com/teste/listatarefas/ListaTarefas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.teste.listatarefas;
+
+/**
+ */
+public class ListaTarefas {
+
+    public static void main(String[] args) {
+        CadastrarTarefas cadastrar = new CadastrarTarefas();
+        cadastrar.main(args);
+    }
+}
